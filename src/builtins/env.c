2472d3e6bcb952ea05b48c219be2ea84c565ea81
Marwan Amine <maamine@2D1.42angouleme.fr>
2024-06-13 15:33:47 +0200

    Builtins debut

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maamine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 16:40:26 by egomez            #+#    #+#             */
-/*   Updated: 2024/06/13 10:13:23 by maamine          ###   ########.fr       */
+/*   Created: 2024/06/13 09:36:04 by maamine           #+#    #+#             */
+/*   Updated: 2024/06/13 15:32:35 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../../includes/minishell.h"
 
-#include <stdio.h>
-#include <libft.h>
-#include <parsing.h>
-#include <expand.h>
-#include <exec.h>
-#include <builtins.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
-# endif
+int	env(int argc, char **argv, char **envp)
+{
+	
+}
