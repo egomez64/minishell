24b14284736102775471744fca35d2f75722e89b
Marwan Amine <maamine@1F5.42angouleme.fr>
2024-06-24 17:50:39 +0200

    malloc secu

--- src/expand/expand_utils.c
+++ src/expand/expand_utils.c
@@ -19,8 +19,13 @@ t_list	*ft_lstnew_empty(void)
 	new = malloc(sizeof (t_list));
 	if (!new)
 		return (NULL);
-	new->content = malloc(sizeof(char));
-	new->content[0] = 0;
+	new->content = malloc(sizeof (char));
+	if (!new->content)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->content[0] = '\0';
 	new->next = NULL;
 	return (new);
 }
@@ -29,53 +34,53 @@ int	is_env(char *s)
 {
 	int	i;
 
-    i = 0;
-    while (s[i] != 0 && s[i] != '$')
-        i++;
-    if (s[i] != 0)
-        return (1);
-    return (0);
+	i = 0;
+	while (s[i] != 0 && s[i] != '$')
+		i++;
+	if (s[i] != 0)
+		return (1);
+	return (0);
 }
 
 bool	is_delimiter(char c)
 {
-    if (ft_isalnum(c))
-        return (false);
-    if (c == '_')
-        return (false);
-    return (true);
+	if (ft_isalnum(c))
+		return (false);
+	if (c == '_')
+		return (false);
+	return (true);
 }
 
 bool	is_whitespace(char c)
 {
-    if ((c >= 7 && c <= 13) || c == 32)
-        return (true);
-    return (false);
+	if ((c >= 7 && c <= 13) || c == 32)
+		return (true);
+	return (false);
 }
 
 char	*ft_strjoin_char(char *s1, char c, bool free_data)
 {
-    int		tot_size;
-    char	*newchain;
+	int		tot_size;
+	char	*newchain;
 	int		i;
 
-    if (!s1)
-        return (NULL);
-    tot_size = ft_strlen(s1) + 2;
-    if (tot_size == 0)
-        return (NULL);
-    newchain = ft_calloc((size_t)tot_size, sizeof(char));
-    if (newchain == NULL)
-        return (NULL);
-    i = 0;
+	if (!s1)
+		return (NULL);
+	tot_size = ft_strlen(s1) + 2;
+	// if (tot_size == 0)		// It won't ever be 0! ft_strlen cannot output -2!
+	// 	return (NULL);
+	newchain = ft_calloc((size_t)tot_size, sizeof(char));
+	if (newchain == NULL)
+		return (NULL);
+	i = 0;
 	while (s1[i])
 	{
-        newchain[i] = s1[i];
-        i++;
-    }
-    newchain[i++] = c;
-    newchain[i] = 0;
-    if (free_data)
-        free(s1);
-    return (newchain);
+		newchain[i] = s1[i];
+		i++;
+	}
+	newchain[i++] = c;
+	newchain[i] = 0;
+	if (free_data)
+		free(s1);
+	return (newchain);
 }
