ac0c073dd545b468cc0540ae7a7aa16d428c0ee0
Marwan Amine <maamine@2G2.42angouleme.fr>
2024-06-28 14:02:19 +0200

    Temporary signal handling so that the project compiles.

--- src/exec/sign.c
+++ src/exec/sign.c
@@ -3,24 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   sign.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: egomez <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: maamine <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 03:59:15 by egomez            #+#    #+#             */
-/*   Updated: 2024/06/28 03:59:20 by egomez           ###   ########.fr       */
+/*   Updated: 2024/06/28 14:01:25 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	handle_signals(int signal)
+int	handle_signals(int signal, int exit_status)
 {
 	if (signal == SIGINT)
 	{
-		write(stderr, "^C\n", 3);
+		write(2, "^C\n", 4);
 		return (130);
 	}
 	else if (signal == SIGQUIT)
 	{
-		write(stderr, "")
+		// write(2, "", 1);
+		// return (131);	// Unfinished
+		write(2, "exit\n", 6);
+		exit(exit_status);
 	}
+	return (0);			// Just to return something
 }
+
+// int	handle_signals(int signal)
+// {
+// 	if (signal == SIGINT)
+// 	{
+// 		write(stderr, "^C\n", 3);
+// 		return (130);
+// 	}
+// 	else if (signal == SIGQUIT)
+// 	{
+// 		write(stderr, "")
+// 	}
+// }
