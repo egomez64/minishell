c1882c7aa6b0074e7d7f9a3bdfbae2c390dcb347
Marwan Amine <maamine@2G2.42angouleme.fr>
2024-07-03 20:36:41 +0200

    cd error message

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maamine <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 09:35:02 by maamine           #+#    #+#             */
-/*   Updated: 2024/06/30 17:14:11 by maamine          ###   ########.fr       */
+/*   Updated: 2024/07/03 20:35:44 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,35 @@ static int	cd_home(t_env *envi)
 	return (ret);
 }
 
+static void	error_message(char *arg, int err)
+{
+	char	*str;
+	char	*message;
+	int		path_len;
+	int		message_len;
+
+	message = strerror(err);
+	if (!message)
+		return ;
+	path_len = ft_strlen(arg);
+	message_len = ft_strlen(message);
+	str = malloc((19 + path_len + message_len) * sizeof (char));
+	if (!str)
+		return ;
+	ft_strlcpy(str, "minishell: cd: ", 16);
+	ft_strlcpy(str + 15, arg, path_len + 1);
+	ft_strlcpy(str + 15 + path_len, ": ", 3);
+	ft_strlcpy(str + 17 + path_len, message, message_len + 1);
+	ft_strlcpy(str + 17 + path_len + message_len, "\n", 2);
+	write(2, str, ft_strlen(str));
+	// free(message);
+	free(str);
+}
+
 int	cd(t_cmd *cmd, t_env *envi)
 {
 	int	ret;
-	int	err;
+	// int	err;
 	int	argc;
 
 	argc = ft_lstsize(cmd->arguments);
@@ -47,9 +72,10 @@ int	cd(t_cmd *cmd, t_env *envi)
 		ret = chdir((char *) cmd->arguments->next->content);
 	if (ret == -1)
 	{
-		err = errno;
-		perror("minishell: cd");
-		return (err);
+		error_message(cmd->arguments->next->content, errno);
+		// err = errno;
+		// perror("minishell: cd");
+		return (1);
 	}
 	return (ret);
 }
