b8221a89c7007fb5d9fd0d05dd73c74d9b7361cd
Marwan Amine <maamine@1G5.42angouleme.fr>
2024-07-28 22:06:16 +0200

    `sig_exec()` becomes useless?

--- src/exec/sign.c
+++ src/exec/sign.c
@@ -6,7 +6,7 @@
 /*   By: maamine <maamine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 03:59:15 by egomez            #+#    #+#             */
-/*   Updated: 2024/07/04 17:34:01 by maamine          ###   ########.fr       */
+/*   Updated: 2024/07/28 21:31:31 by maamine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,17 @@ void	heredoc_c(int signal)
 	rl_replace_line("", 0);
 }
 
-void	sig_exec(int wstatus)
-{
-	int	signal;
-
-	signal = WTERMSIG(wstatus);
-	if (signal == SIGINT)
-	{
-		// normal_c(signal);
-		printf("\n");
-	}
-}
+// void	sig_exec(int wstatus)
+// {
+// 	int	signal;
+// 
+// 	signal = WTERMSIG(wstatus);
+// 	if (signal == SIGINT)
+// 	{
+// 		normal_c(signal);
+// 		printf("\n");
+// 	}
+// }
 
 // int	sig_exec(int wstatus/*, bool is_last*/)
 // {
